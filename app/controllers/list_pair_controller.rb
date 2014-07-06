b622da1c865684baf165f02eb71fea9789dde6ed
David Tresner-Kirsch <dwkirsch@gmail.com>
2014-07-06 17:23:29 -0400

    completed list pair controller

--- app/controllers/list_pair_controller.rb
+++ app/controllers/list_pair_controller.rb
@@ -1,19 +1,21 @@
 class ListPairController < ApplicationController
   respond_to :json
   require "medrec/list_pair_reader_from_string.rb"
-  def align
-#this works as a post with reply for prescriptions#index, take inspiration from this 
-#    @prescription.values = puts 
-#    reader = PrescriptionObjectReader.new(@prescription)
-#    @pipeline = Pipeline.new(reader, [MyFeatures.new, AllErrorsClassifier.new])
-#    @pipeline.run 1
-#    @prescription.features = nil # sanitize artifacts other than the input and the decision. make this less kludgey later.
-#    respond_to do |format|
-#      format.json {render :json => @prescription}
-#    end
 
+  def align #POST
+    reader = ListPairReaderFromString.new(params[:listpair])
+    pipeline_array = [ListPairPrescriptionFeatureExtractor.new, Aligner.new, \
+                      AlignedListStrictStringMatchDiscrepancyDetector.new(["name", "annotation"]), \
+                      AlignedListMedAttribMatchDiscrepancyDetector.new, \
+                      AlignedListDoseStrengthDiscrepancyDetector.new(["dose"]),\
+                      GUIMockup.new ]
+    @pipeline = Pipeline.new(reader, pipeline_array)
+    @pipeline.run 1
 
-    reader = ListPairReaderFromString.new
+    respond_to do |format|
+      format.json {render :json => {"medsRecon" => @pipeline.data.first.values["medsRecon"]}}
+    end
+
   end
 
 end
