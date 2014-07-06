a52c68deef59bfa2cd2a49de371999938d60ce50
David Tresner-Kirsch <dwkirsch@gmail.com>
2014-07-06 14:41:32 -0400

    potential switch to post-only

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -4,6 +4,15 @@ class PrescriptionsController < ApplicationController
   #API posts a JSON object to index...
   #e.g. curl -d 'prescription[values]={"Medication":"ADVAIR DISKUS 100/50 (FLUTICASONE PROPIONATE/SALMETEROL 100/50)","Route":"INH","Dose":"1","DoseUnits":"PUFF","Strength":"","Take":"","Form":"","Frequency":"daily","PRN":"0","PRNreason":"","Duration":"","DurationUnits":"","Dispensequantity":"1","Dispensequantityunits":"diskus","DIRECTIONS":"do not exceed the prescribed dosage"}' -i localhost:3000/prescriptions/index.json -L
   def index
+#    @prescription = Prescription.new
+#    @prescription.values = JSON.parse(params[:prescription][:values])
+#    reader = PrescriptionObjectReader.new(@prescription)
+#    @pipeline = Pipeline.new(reader, [MyFeatures.new, AllErrorsClassifier.new])
+#    @pipeline.run 1
+#    @prescription.features = nil # sanitize artifacts other than the input and the decision. make this less kludgey later.
+#    respond_to do |format|
+#      format.json {render :json => @prescription}
+#    end
     respond_to do |format|
       format.json {redirect_to :action => "home", :prescription_values => params[:prescription][:values] }
     end
