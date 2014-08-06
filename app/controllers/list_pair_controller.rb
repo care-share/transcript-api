82f198ea5fae6e3bd5fdc4d6e58fbbcba1fd073a
David Tresner-Kirsch <dwkirsch@gmail.com>
2014-08-06 16:58:55 -0400

    make rails ready to play nice with posts from angular

--- app/controllers/list_pair_controller.rb
+++ app/controllers/list_pair_controller.rb
@@ -2,6 +2,17 @@ class ListPairController < ApplicationController
   respond_to :json
   require "medrec/list_pair_reader_from_string.rb"
 
+  before_filter :set_access_control
+
+  def set_access_control
+    headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+    headers['Access-Control-Expose-Headers'] = 'ETag'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+    headers['Access-Control-Allow-Headers'] = '*,x-requested-with,Content-Type,If-Modified-Since,If-None-Match,Auth-User-Token'
+    headers['Access-Control-Max-Age'] = '86400'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+  end
+
   def align #POST
     reader = ListPairReaderFromString.new(params[:listpair])
     pipeline_array = [ListPairPrescriptionFeatureExtractor.new, Aligner.new, \
