8e32c7cf819e9d16b09584fbf2afb7559213f237
David Tresner-Kirsch <davidtk@mitre.org>
2015-12-30 15:06:03 -0500

    examples and write recon object to log

--- app/controllers/list_pair_controller.rb
+++ app/controllers/list_pair_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class ListPairController < ApplicationController
   respond_to :json
   require "medrec/list_pair_reader_from_string.rb"
@@ -25,10 +26,18 @@ class ListPairController < ApplicationController
     @pipeline = Pipeline.new(reader, pipeline_array)
     @pipeline.run 1
 
+    puts @pipeline.data.first.values["medsRecon"].inspect
+
     respond_to do |format|
       format.json {render :json => {"medsRecon" => @pipeline.data.first.values["medsRecon"]}}
     end
-
   end
 
+################# Example Usage ########################################
+# Simulated aligner call:
+# curl -O --data @/home/davidtk/transcript/git/transcript-rule-based-full/merged_short__for_service__post.json -i http://localhost:3005/list_pair/align.json -L --verbose
+#
+# Contents of listpair.json:
+# listpair={ "patientId": "1", "hh": [ { "patientId": "1", "name": "RABEPRAZOLE NA 20MG EC TAB", "form": null, "dose": "20MG", "SIG": "BID", "route": "ORAL", "instructions": null, "annotation": null, "id": 1 }, { "patientId": "1", "name": "DOCUSATE NA 100MG CAP", "form": null, "dose": "200MG", "SIG": "BID", "route": "ORAL", "instructions": null, "annotation": null, "id": 2 }, { "patientId": "1", "name": "IBUPROFEN 400MG TAB", "form": null, "dose": "400MG", "SIG": "TID", "route": "ORAL", "instructions": null, "annotation": null, "id": 3 }, { "patientId": "1", "name": "LISINOPRIL 5MG TAB", "form": null, "dose": "5MG", "SIG": "QD", "route": "ORAL", "instructions": null, "annotation": null, "id": 4 }, { "patientId": "1", "name": "SERTRALINE HCL 100MG TAB", "form": null, "dose": "50MG", "SIG": "QD", "route": "ORAL", "instructions": null, "annotation": null, "id": 5 }, { "patientId": "1", "name": "SIMVASTATIN 5MG TAB", "form": null, "dose": "5MG", "SIG": "QPM", "route": "ORAL", "instructions": null, "annotation": null, "id": 6 }, { "patientId": "1", "name": "SENNOSIDES 8.6MG TAB", "form": null, "dose": "8.6MG", "SIG": "BID PRN", "route": "ORAL", "instructions": null, "annotation": null, "id": 7 }, { "patientId": "1", "name": "TERAZOSIN HCL 10MG CAP", "form": null, "dose": "10MG", "SIG": "QHS", "route": "ORAL", "instructions": null, "annotation": null, "id": 8 }, { "patientId": "1", "name": "ALENDRONATE 70MG TAB", "form": null, "dose": "70MG", "SIG": "QWEEK", "route": "ORAL", "instructions": null, "annotation": null, "id": 9 }, { "patientId": "1", "name": "WARFARIN NA (GOLDEN STATE) 5MG TAB", "form": null, "dose": "5MG", "SIG": "Q-PM", "route": "ORAL", "instructions": null, "annotation": null, "id": 10 } ], "va": [ { "patientId": "1", "name": "Acetaminophen", "form": null, "dose": "1000mg", "SIG": "q6º", "route": "po", "instructions": null, "annotation": null, "id": 1 }, { "patientId": "1", "name": "Albuterol", "form": null, "dose": "90mcg/200 2 puffs", "SIG": "Q4ºPRN", "route": "inhale", "instructions": null, "annotation": null, "id": 2 }, { "patientId": "1", "name": "albuterol 90/1Pratrop", "form": null, "dose": "2puffs", "SIG": "Q6ºPRN", "route": "inhale", "instructions": null, "annotation": null, "id": 3 }, { "patientId": "1", "name": "Alendronate", "form": null, "dose": "70mg", "SIG": "QMONDAY", "route": "po", "instructions": null, "annotation": null, "id": 4 }, { "patientId": "1", "name": "Calcium-Vitamin D", "form": null, "dose": "1tab", "SIG": "TID w/meals", "route": "po", "instructions": null, "annotation": null, "id": 5 }, { "patientId": "1", "name": "Coumadin", "form": null, "dose": "5mg", "SIG": "QPM", "route": "po", "instructions": null, "annotation": null, "id": 6 }, { "patientId": "1", "name": "Advil", "form": null, "dose": "400mg", "SIG": "TID w/meals", "route": "po", "instructions": null, "annotation": null, "id": 7 }, { "patientId": "1", "name": "Lisinopril", "form": null, "dose": "5mg", "SIG": "QD", "route": "po", "instructions": null, "annotation": null, "id": 8 }, { "patientId": "1", "name": "Morphine Sulfate", "form": null, "dose": "15mg", "SIG": "Q6ºPRN", "route": "po", "instructions": null, "annotation": null, "id": 9 }, { "patientId": "1", "name": "raberprazole", "form": null, "dose": "20 mg", "SIG": "bid 30min prior to meal", "route": "po", "instructions": null, "annotation": null, "id": 10 }, { "patientId": "1", "name": "Sertraline HCL", "form": null, "dose": "50 mg", "SIG": "QD", "route": "po", "instructions": null, "annotation": null, "id": 11 }, { "patientId": "1", "name": "simvastin", "form": null, "dose": "5mg", "SIG": "QPM", "route": "po", "instructions": null, "annotation": null, "id": 12 }, { "patientId": "1", "name": "TERAZOSIN", "form": null, "dose": "10mg", "SIG": "QPM", "route": "po", "instructions": null, "annotation": null, "id": 13 }, { "patientId": "1", "name": "Vitamin D", "form": null, "dose": "200u", "SIG": "TID w/meals", "route": "po", "instructions": null, "annotation": null, "id": 14 } ] }
+#########################################################################
 end
